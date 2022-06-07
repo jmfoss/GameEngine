40f82008274c91d76510aacbdacd84ed8aff126c
Joshua Foss <jmfoss98@gmail.com>
2022-06-07 15:14:50 -0400

    Implement spdlog wrapper

--- GameEngine/src/GameEngine/EntryPoint.h
+++ GameEngine/src/GameEngine/EntryPoint.h
@@ -7,6 +7,8 @@ extern GameEngine::Application* GameEngine::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	GameEngine::Log::Init();
+
 	auto app = GameEngine::CreateApplication();
 	app->Run();
 	delete app;
